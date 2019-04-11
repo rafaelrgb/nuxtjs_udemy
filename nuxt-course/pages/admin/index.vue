bc07c316f3ae4818181eec4fbde375927e968ade
Rafael Gomes Braga <faerugb@gmail.com>
2019-04-11 20:47:29 -0300

    New components for the admin page

--- nuxt-course/pages/admin/index.vue
+++ nuxt-course/pages/admin/index.vue
@@ -19,3 +19,19 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.admin-page {
+    padding: 20px;
+}
+
+.new-post {
+    text-align: center;
+    border-bottom: 2px solid #ccc;
+    padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+    text-align: center;
+}
+</style>
