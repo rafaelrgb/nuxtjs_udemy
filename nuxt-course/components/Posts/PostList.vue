bcd07eafa8d6d32adfcca5faa9927221ca19c19a
Rafael Gomes Braga <faerugb@gmail.com>
2019-04-12 11:19:07 -0300

    finished basic blog application

--- nuxt-course/components/Posts/PostList.vue
+++ nuxt-course/components/Posts/PostList.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="posts-list">
+    <div class="posts-list">
         <section class="post-list">
             <PostPreview
                 id="1"
+                :is-admin="isAdmin"
                 thumbnail="https://www.thomsonreuters.com/content/dam/ewp-m/images/image-library/en/photography/201138-118072186.jpeg"
                 title="Hello there!"
                 previewText="This is my first post!"
             />
             <PostPreview
                 id="2"
+                :is-admin="isAdmin"
                 thumbnail="https://www.thomsonreuters.com/content/dam/ewp-m/images/image-library/en/photography/201138-118072186.jpeg"
                 title="Hello there!"
                 previewText="This is my second post!"
             />
             <PostPreview
                 id="3"
+                :is-admin="isAdmin"
                 thumbnail="https://www.thomsonreuters.com/content/dam/ewp-m/images/image-library/en/photography/201138-118072186.jpeg"
                 title="Hello there!"
                 previewText="This is my third post!"
@@ -29,6 +32,12 @@ import PostPreview from "@/components/Posts/PostPreview.vue";
 export default {
     components: {
         PostPreview
+    },
+    props: {
+        isAdmin: {
+            type: Boolean,
+            default: false
+        }
     }
 };
 </script>
---
+++ nuxt-course/pages/admin/_postId/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost"/>
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm";
+
+export default {
+    layout: "admin",
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: "Rafael",
+                title: "My Awesome Post",
+                content: "Super amazing thanks for that!",
+                thumbnailLink:
+                    "https://www.thomsonreuters.com/content/dam/ewp-m/images/image-library/en/photography/201138-118072186.jpeg"
+            }
+        };
+    }
+};
+</script>
+
+<style scoped>
+.update-form {
+    width: 90%;
+    margin: 20px auto;
+}
+@media (min-width: 768px) {
+    .update-form {
+        width: 500px;
+    }
+}
+</style>
+
